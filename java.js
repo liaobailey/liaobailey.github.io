dfa6401557890e8e4861730e6b45f17599e3feac
liaobailey <47491229+liaobailey@users.noreply.github.com>
2020-08-25 19:08:15 -0400

    Update java.js

--- java.js
+++ java.js
@@ -15,6 +15,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  var dots = document.getElementsByClassName("caption-container");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
